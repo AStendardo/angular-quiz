fa8b8d3b277a4909ffc7cad0b59468cdf9ac29b3
Andrea Stendardo <hello@andreastendardo.dev>
2026-03-15 12:49:09 +0100

    Initial commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'question/:idx',
+    renderMode: RenderMode.Client // Dice ad Angular di non prerenderizzare questa rotta
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender // Mantiene il prerender per il resto dell'app
   }
 ];
